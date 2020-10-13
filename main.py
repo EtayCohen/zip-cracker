f2b5ee7e72831befad1a751f33636b55f8ce202b
Etay Cohen <etaychn@gmail.com>
2020-10-13 10:48:45 +0300

    Added support for arguments

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import sys
 import zipfile
 
-PATH = 'password.zip'
-DIGITS = 4
 
-for x in range(pow(10, DIGITS)):
-    password = str(x).zfill(DIGITS)
-    with zipfile.ZipFile(PATH) as file:
-        try:
-            print(password)
-            file.extractall(pwd=password.encode(), path='extracted')
-            break
-        except Exception as e:
-            pass
+def crack(path, num_of_digits):
+    for x in range(pow(10, num_of_digits)):
+        password = str(x).zfill(num_of_digits)
+        with zipfile.ZipFile(path) as file:
+            try:
+                print(password)
+                file.extractall(pwd=password.encode(), path='extracted')
+                break
+            except Exception as e:
+                pass
+
+
+def main():
+    if len(sys.argv) > 2:
+        crack(sys.argv[1], int(sys.argv[2]))
+    else:
+        print("USAGE: python main.py path num_of_digits")
+
+
+if __name__ == '__main__':
+    main()
